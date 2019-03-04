8294dcd8f05aee78e3f2766a5dc61093b68899a7
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-04 15:13:15 -0800

    Start working on JS interactivity

--- app/assets/javascripts/interactivity.js
+++ app/assets/javascripts/interactivity.js
@@ -1,2 +1,12 @@
 
 console.log("Hello!")
+
+document.querySelectorAll('i.fa').forEach((node) =>
+		node.addEventListener('click', (event) => {
+            console.log('Page has been clicked!');
+            //const descriptionNode = document.createElement('p');
+            //const h1 = document.createElement('h1');
+            //h1.innerText = `${@idea.description}`;
+            //descriptionNode.append(h1);
+          })
+);
