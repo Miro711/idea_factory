2f4bac3f7a5a3a5cadfa28ded40c4050402e6a83
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-04 10:18:46 -0800

    Define edit & update actions with edit view template and add link to edit idea on its show page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,31 +1,44 @@
 class IdeasController < ApplicationController
+  def new
+    @idea = Idea.new
+  end
 
-    def new
-        @idea = Idea.new
+  def create
+    idea_params = params.require(:idea).permit(:title, :description)
+    @idea = Idea.new idea_params
+    if @idea.save
+      redirect_to idea_path(@idea)
+    else
+      render :new
     end
+  end
 
-    def create
-        idea_params = params.require(:idea).permit(:title, :description)
-        @idea = Idea.new idea_params
-        if @idea.save
-            redirect_to idea_path(@idea)
-        else
-            render :new
-        end
-    end
+  def show
+    @idea = Idea.find params[:id]
+  end
 
-    def show
-        @idea = Idea.find params[:id]
-    end
+  def index
+    @ideas = Idea.order(created_at: :DESC)
+  end
 
-    def index
-        @ideas = Idea.order(created_at: :DESC)
-    end
-    
-    def destroy
-        @idea = Idea.find(params[:id])
-        @idea.destroy
-        redirect_to ideas_path
-    end
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    redirect_to ideas_path
+  end
 
+  def edit
+    @idea = Idea.find params[:id]
+  end
+
+  def update
+    idea_params = params.require(:idea).permit(:title, :description)
+    @idea = Idea.find params[:id]
+    if @idea.update idea_params
+      redirect_to idea_path(@idea)
+    else
+      render :edit
+    end
+  end
+  
 end
