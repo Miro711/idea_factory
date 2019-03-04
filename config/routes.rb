9d7f36453f0102c99bb3776a343648c80ed57614
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-04 11:27:37 -0800

    Generate users controller, define users resources & actions for new & create with new view template for sign up form and add link to sign up page in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "/", to: "ideas#index", as: :root
+  resources :users, only: [:new, :create]
   resources :ideas do
     resources :reviews, shallow: true, only: [:create, :destroy]
   end
