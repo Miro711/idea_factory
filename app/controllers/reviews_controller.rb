830dadf7081b9eb05bb7cda4082486936a9d64c8
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-04 10:54:48 -0800

    Define reviews resources & action for destroying review and add delete review link to each review on show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,25 @@
 class ReviewsController < ApplicationController
+  def create
+    @idea = Idea.find(params[:idea_id])
+    @review = Review.new review_params
+    @review.idea = @idea
+    if @review.save
+      redirect_to @idea
+    else
+      @reviews = @ideas.reviews.order(created_at: :desc)
+      render "ideas/show"
+    end
+  end
 
-    def create
-        @idea = Idea.find(params[:idea_id])
-        @review = Review.new review_params
-        @review.idea = @idea
-        if @review.save
-          redirect_to @idea
-        else
-          @reviews = @ideas.reviews.order(created_at: :desc)
-          render 'ideas/show'
-        end
-      end
-    
-      private
-    
-      def review_params
-        params.require(:review).permit(:body)
-      end
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to @review.idea
+  end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :ideas do
-    resources :reviews, shallow: true, only: [:create]
+    resources :reviews, shallow: true, only: [:create, :destroy]
   end
 end
